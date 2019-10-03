22342dfd9bfa5fa64eb60adc78e91caed9429f52
mishale22 <mishale181@gmail.com>
2019-10-02 19:12:59 -0500

    travis

--- test_demo.py
+++ test_demo.py
@@ -7,5 +7,7 @@ def test_add():
 # THEN I get the addition of two numbers
     add_result = demo.add(3,4)
     assert add_result == 7
+    assert demo.add(1,10) == 11
+    assert demo.add(2,4) == 6
 
 
