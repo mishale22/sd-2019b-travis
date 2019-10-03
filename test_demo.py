8479e1023242fd26b433714f56c4ae7bf2cf045c
mishale22 <mishale181@gmail.com>
2019-10-02 19:20:53 -0500

    travis sub

--- test_demo.py
+++ test_demo.py
@@ -10,4 +10,12 @@ def test_add():
     assert demo.add(1,10) == 11
     assert demo.add(2,4) == 6
 
+def test_sub():
+# GIVEN two numbers
+# WHEN I invoke the method
+# THEN I get the addition of two numbers
+    assert demo.sub(3,4) == -1
+    assert demo.sub(1,9) == -8
+    assert demo.sub(2,4) == -2
+
 
